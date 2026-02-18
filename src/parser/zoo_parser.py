f07de09fa4dba0eebf0c3d1036767c01a9f50e21
lia <s9529425@matzov.idf>
2026-02-18 13:18:57 +0200

    fixing

--- src/parser/zoo_parser.py
+++ src/parser/zoo_parser.py
@@ -4,9 +4,19 @@ from src.parser.input_parser import InputParser
 
 
 class ZooParser(InputParser):
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(ZooParser, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("filepath", help="Please enter file path to read animal names from")
+        if not self._initialized:
+            self.parser = argparse.ArgumentParser()
+            self.parser.add_argument("filepath", help="Please enter file path to read animal names from")
+            self._initialized = True
 
     def parse_to_words(self) -> dict:
         """
