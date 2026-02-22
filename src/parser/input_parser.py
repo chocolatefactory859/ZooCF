2a61c525b78c3635237f3fcb98a054c3912500d3
lia <s9529425@matzov.idf>
2026-02-22 14:31:18 +0200

    added singleton meta class

--- src/parser/input_parser.py
+++ src/parser/input_parser.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 
+from src.parser.singleton_meta import SingletonMeta
 
-class InputParser(ABC):
+
+class InputParser(metaclass=SingletonMeta):
     @abstractmethod
     def parse_input(self):
         pass
--- src/parser/zoo_parser.py
+++ src/parser/zoo_parser.py
@@ -4,19 +4,10 @@ from src.parser.input_parser import InputParser
 
 
 class ZooParser(InputParser):
-    _instance = None
-    _initialized = False
-
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(ZooParser, cls).__new__(cls)
-        return cls._instance
-
     def __init__(self):
-        if not self._initialized:
-            self.parser = argparse.ArgumentParser()
-            self.parser.add_argument("filepath", help="Please enter file path to read animal names from")
-            self._initialized = True
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("filepath", help="Please enter file path to read animal names from")
+        self._initialized = True
 
     def parse_input(self) -> dict:
         """
